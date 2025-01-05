4c7dc564bf10ea80aaeba91f31ffb2319a612f04
loubna213 <loub60418@gmail.com>
2025-01-05 14:55:17 +0100

    add shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,32 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primarey: {
+  	extend: {
+  		colors: {
+  			primarey: {
   				pink: {
-  					DEFAULT: '#FECCFF',
   					'100': '#FFCCCC',
-  					'500': '#FF547D'
+  					'500': '#FF547D',
+  					DEFAULT: '#FECCFF'
   				},
   				purple: {
-  					DEFAULT: '#686EFF',
-  					'100': '#CDC9FF'
+  					'100': '#CDC9FF',
+  					DEFAULT: '#686EFF'
   				},
   				orange: {
-  					DEFAULT: '#FFEBCC',
-  					'100': '#FFF9F5'
+  					'100': '#FFF9F5',
+  					DEFAULT: '#FFEBCC'
   				},
   				blue: {
-  					DEFAULT: '#CCE7FF',
-  					'100': '#F5F8FF'
+  					'100': '#F5F8FF',
+  					DEFAULT: '#CCE7FF'
   				},
   				green: {
   					DEFAULT: '#cdffcc'
@@ -35,15 +36,58 @@ export default {
   				white: {
   					'50': '#F5F7F9',
   					'100': '#EBEBEB',
-  					'200': '#D8D8D8"?\n            "300": "#CECECE'
+  					'200': '#D8D8D8"?\\n            "300": "#CECECE'
   				},
   				gray: {
   					'400': '#5A5A5A',
   					'500': '#4F4F4F'
   				},
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
   			},
-      },
-    },
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
